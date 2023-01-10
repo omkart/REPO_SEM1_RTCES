1eb402d864ec90818951b0d68116341a094a178e
Omkar Tulaskar <omkar.tulaskar97@gmail.com>
2023-01-10 23:55:40 +0530

    save ex 3 main project file

---
+++ exercise03_IPC-App/FreeRTOS/RTOS_IPC/WIN32-MSVC/main_exercise.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * NOTE: Windows will not be running the FreeRTOS demo threads continuously, so
+ * do not expect to get real time behaviour from the FreeRTOS Windows port, or
+ * this demo application.  Also, the timing information in the FreeRTOS+Trace
+ * logs have no meaningful units.  See the documentation page for the Windows
+ * port for further information:
+ * http://www.freertos.org/FreeRTOS-Windows-Simulator-Emulator-for-Visual-Studio-and-Eclipse-MingW.html
+
+/* Standard includes. */
+#include <stdio.h>
+#include <assert.h>
+#include <conio.h>
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "semphr.h"
+
+/* TODO */
+
+void main_exercise( void )
+{
+
+	printf("Hello world");
+	/* TODO */
+}
+/*-----------------------------------------------------------*/
+
+
+/* TODO */
+/*-----------------------------------------------------------*/
+
+
